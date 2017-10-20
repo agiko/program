2e567cf8af0442d956bbcec76b6c4d954fb98548
agiko <agiko2@o2.pl>
2017-10-20 14:53:23 +0200

    program3b

---
+++ program3b.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int mnoz(int a, int b){
+	printf("wynik mnozenia: %i\n", a * b);
+	return a * b; }
+int dodaj (int a, int b){
+	printf("wynik dodawania: %i\n", a + b);
+	return a + b; }
+int dziel (int a, int b){
+	printf("wynik dzielenia: %i\n", a / b);
+	return a / b; }
+int odejmij (int a, int b){
+	printf("wynik odejmowania: %i\n", a -b);
+	return a - b; }
+
+int main(void)
+{
+	int a = 33, b = 11;
+	char wybor ;
+	printf("Podaj dzialania: "); scanf("%c", &wybor);
+	printf("Podaj wartosc argumentu a: "); scanf("%i", &a);
+	printf("Podaj wartosc argumentu b: "); scanf("%i", &b);
+
+
+	if(wybor == '*'){
+	mnoz (a, b);
+	}
+	else if(wybor == '/'){
+	dziel (a, b);
+	}
+	else if(wybor == '+'){
+	dodaj (a, b);
+	}
+	else if(wybor == '-'){
+	odejmij (a, b);
+	}
+	return 0;
+}
+
